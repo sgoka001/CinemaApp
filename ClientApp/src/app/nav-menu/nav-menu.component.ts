a99115254b0af115809b9c004691742082ceafd3
Srisri Gokanapudy <srisri.gokanapudy.382@my.csun.edu>
2019-12-01 15:04:39 -0800

    Added auth0 authentication

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -15,4 +16,10 @@ export class NavMenuComponent {
   toggle() {
     this.isExpanded = !this.isExpanded;
     }
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {
+    }
+
 }
